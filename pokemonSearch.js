106233d6ed8ac1dad9868015e057c509a9eaa8b5
amjadsh92 <amjadsharafeddien@gmail.com>
2025-02-12 21:10:43 +0200

    Manipulate the DOM

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -1,3 +1,19 @@
+const pokemonToFind = document.getElementById('input-pokemon');
+const searchButton = document.getElementById('search-btn');
+const pokemonName = document.getElementById('pokemon-name');
+const pokemonId = document.getElementById('pokemon-id')
+const pokemonCharacter = document.getElementById('pokemon-character')
+const weightAndHeight = document.getElementById('weight-height')
+const pokemonImage = document.getElementById('pokemon-image')
+const strength1 = document.getElementById('strength1');
+const strength2 = document.getElementById('strength2');
+const hp = document.getElementById('stat-hp');
+const attack = document.getElementById('stat-attack');
+const defense = document.getElementById('stat-defense');
+const spAttack = document.getElementById('stat-spAttack');
+const spDefense = document.getElementById('stat-spDefense');
+const speed = document.getElementById('stat-speed');
+
 let pokemonCharacters;
 
 
@@ -41,7 +57,7 @@ class pokemonCharacter {
        // this.exist = (idOrName) => this.getPokemonCharacter(idOrName)
     }
 
-    getPokemonCharacter = async (idOrName) => {
+    exist = async (idOrName) => {
         for (let pokemonCharacter of pokemonCharacters){
             if (pokemonCharacter.id == idOrName || pokemonCharacter.name == idOrName){
                 const characteristics = await fetchData(pokemonCharacter.url)
@@ -56,8 +72,8 @@ class pokemonCharacter {
                 this.spAttack += characteristics.stats[3].base_stat
                 this.spDefense += characteristics.stats[4].base_stat
                 this.speed += characteristics.stats[5].base_stat
-                this.strength1 += characteristics?.types[0]?.type?.name
-                this.strength2 += characteristics?.types[1]?.type?.name
+                this.strength1 = characteristics?.types[0] ? characteristics?.types[0]?.type?.name : this.strength1
+                this.strength2 = characteristics?.types[1] ? characteristics?.types[1]?.type?.name : this.strength2
             
 
 
@@ -72,9 +88,9 @@ class pokemonCharacter {
 
 }  
 
-pokemon = new pokemonCharacter()
+const pokemon = new pokemonCharacter()
 
-const found = await pokemon.getPokemonCharacter(idOrName)
+const found = await pokemon.exist(idOrName)
 if(found){
     
     return pokemon
@@ -92,22 +108,174 @@ else{
 
 }
 
-getResult = async() => {
+const getPokemonCharacter = async(idOrName) => {
 
     await getPokemonCharacters()
 
-    pokemon = await getPokemon(4)
+     const pokemon = await getPokemon(idOrName)
 
-    console.log(pokemon)
+    return pokemon
     
 
 
   
 }
 
+const showPokemonCharacter = async (idOrName)=>{
+     debugger;
+     const pokemon = await getPokemonCharacter(idOrName)
+     if(pokemon){
+     reset()
+     pokemonName.innerText= `${pokemon.name} #${pokemon.id}`;
+     weightAndHeight.innerText = `Weight: ${pokemon.weight} Height: ${pokemon.height}`
+     pokemonImage.src= `${pokemon.image}`
+     hp.innerText=`${pokemon.hp}`
+     attack.innerText=`${pokemon.attack}`
+     defense.innerText=`${pokemon.defense}`
+     spAttack.innerText=`${pokemon.spAttack}`
+     spDefense.innerText=`${pokemon.spDefense}`
+     speed.innerText=`${pokemon.speed}`
+
+     if(pokemon.strength1){
+        strength1.style.display="block";
+        strength1.innerText = `${pokemon.strength1}`
+        if(pokemon.strength1 == 'grass'){
+            strength1.style.backgroundColor= "#78cc55";
+        }
+        else if(pokemon.strength1 == 'poison'){
+            strength1.style.backgroundColor= "#c68bb7";
+        }
+
+        else if(pokemon.strength1 == 'fire'){
+            strength1.style.backgroundColor= "#ff6f52";
+        }
+        else if(pokemon.strength1 == 'flying'){
+            strength1.style.backgroundColor= "#8899ff";
+        }
+        
+       else if(pokemon.strength1 == 'water'){
+            strength1.style.backgroundColor= "#42a1ff";
+        }
+
+        else if(pokemon.strength1 == 'bug'){
+            strength1.style.backgroundColor="#aabb23";
+        }
+        
+        else if(pokemon.strength1 == 'normal'){
+            strength1.style.backgroundColor="#b7b7aa";
+        }
+        
+        else if(pokemon.strength1 == 'dark'){
+            strength1.style.backgroundColor="#b59682";
+        }
+
+        else if(pokemon.strength1 == 'fighting'){
+            strength1.style.backgroundColor="#d3887e";
+        }
+        
+        else if(pokemon.strength1 == 'dragon'){
+            strength1.style.backgroundColor="#9e93f1";
+        }
+       
+        else if(pokemon.strength1 == 'ice'){
+            strength1.style.backgroundColor="#66ccfe";
+        }
 
 
-getResult()
+     } 
+
+     if(pokemon.strength2){
+        strength2.style.display="block";
+        strength2.innerText = `${pokemon.strength2}`
+        if(pokemon.strength2 == 'grass'){
+            strength2.style.backgroundColor= "#78cc55";
+        }
+        else if(pokemon.strength2 == 'poison'){
+            strength2.style.backgroundColor= "#c68bb7";
+        }
+
+        else if(pokemon.strength2 == 'fire'){
+            strength2.style.backgroundColor= "#ff6f52";
+        }
+        else if(pokemon.strength2 == 'flying'){
+            strength2.style.backgroundColor= "#8899ff";
+        }
+        
+       else if(pokemon.strength2 == 'water'){
+            strength2.style.backgroundColor= "#42a1ff";
+        }
+
+        else if(pokemon.strength2 == 'bug'){
+            strength2.style.backgroundColor="#aabb23";
+        }
+        
+        else if(pokemon.strength2 == 'normal'){
+            strength2.style.backgroundColor="#b7b7aa";
+        }
+        
+        else if(pokemon.strength2 == 'dark'){
+            strength2.style.backgroundColor="#b59682";
+        }
+
+        else if(pokemon.strength2 == 'fighting'){
+            strength2.style.backgroundColor="#d3887e";
+        }
+        
+        else if(pokemon.strength2 == 'dragon'){
+            strength2.style.backgroundColor="#9e93f1";
+        }
+       
+        else if(pokemon.strength2 == 'ice'){
+            strength2.style.backgroundColor="#66ccfe";
+        }
+
+     } 
+
+
+
+
+     
+     
+     pokemonCharacter.classList.remove("hide")
+     
+    }
+    else{
+        alert("Pokémon not found")
+        reset()
+    }
+      
+}
+
+
+const reset =()=>{
+
+    if(!pokemonCharacter.classList.contains("hide")){
+        pokemonCharacter.classList.add("hide")
+
+        hp.innerText= ""
+        attack.innerText= ""
+        defense.innerText= ""
+        spAttack.innerText= ""
+        spDefense.innerText= ""
+        speed.innerText= ""
+        strength1.style.display="none"
+        strength2.style.display="none"
+        strength1.style.backgroundColor= "#b7b7aa"
+        strength2.style.backgroundColor= "#b7b7aa"
+
+    }
+
+
+
+}
+
+
+
+
+searchButton.addEventListener("click", () => showPokemonCharacter(pokemonToFind.value))
+
+
+
 
   
 
